cf37be55578fdd38c2f2976e490a39f5f4a8a5bd
omar <omarasaaam@gmail.com>
2024-05-26 23:55:42 +0300

    fixing search issue editiing the port

--- web_dev/frontend/studentattendance.js
+++ web_dev/frontend/studentattendance.js
@@ -3,7 +3,7 @@
         async function fetchData() {
             let data = [];
             try {
-                const response = await fetch('http://localhost:3000/api/attendance'); // Added http:// to the URL
+                const response = await fetch('http://localhost:5090/api/attendance'); // Added http:// to the URL
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
